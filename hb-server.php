6d032f3ba33ebd3023f44d855d6c9861ee0ce0b9
Ryan Tvenge <ryan@hoverboardstudios.com>
2022-05-11 11:19:45 -0500

    Stopping emails for themes and plugins.

--- hb-server.php
+++ hb-server.php
@@ -10,6 +10,8 @@
  */
 
 add_filter( 'auto_core_update_send_email', '__return_false' );
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
+add_filter( 'auto_theme_update_send_email', '__return_false' );
 define( 'FORCE_SSL_ADMIN', true );
 
 /**
