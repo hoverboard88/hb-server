f9811762f0afd3c3b33d2c84082090cbf79d95ce
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-12-19 09:13:13 -0600

    Adding includes.

--- hb-server.php
+++ hb-server.php
@@ -15,8 +15,16 @@
  * @package hb_server
  */
 
+// set a global varable for the theme directory
+define( 'HB_SERVER_URL', plugin_dir_url( __FILE__ ) );
+define( 'HB_SERVER_DIR', plugin_dir_path( __FILE__ ) );
+
 require plugin_dir_path( __FILE__ ) . '/inc/set-env-vars.php';
 require plugin_dir_path( __FILE__ ) . '/inc/security.php';
 require plugin_dir_path( __FILE__ ) . '/inc/admin-bar-icons.php';
 require plugin_dir_path( __FILE__ ) . '/inc/seo.php';
 require plugin_dir_path( __FILE__ ) . '/inc/env-colors.php';
+require plugin_dir_path( __FILE__ ) . '/inc/nit-picky.php';
+require plugin_dir_path( __FILE__ ) . '/inc/dashboard.php';
+require plugin_dir_path( __FILE__ ) . '/inc/admin.php';
+require plugin_dir_path( __FILE__ ) . '/inc/brand.php';
