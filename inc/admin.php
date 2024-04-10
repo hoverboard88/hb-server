f301894b5f1ace3780e62f5e31c2e5a11f255d12
Ryan Tvenge <ryan@hoverboardstudios.com>
2024-04-10 09:51:42 -0500

    Removing Divi memory allocation.

--- inc/admin.php
+++ inc/admin.php
@@ -71,10 +71,6 @@ add_action( 'admin_enqueue_scripts', 'hb_enqueue_admin_styles' );
  * Increase memory limit for page builders.
  */
 function hb_increase_admin_memory_limit() {
-	if ( defined( 'ET_CORE' ) ) {
-		ini_set( 'memory_limit', '512M' ); // phpcs:ignore
-	}
-
 	if ( defined( 'ELEMENTOR_VERSION' ) ) {
 		ini_set( 'memory_limit', '512M' ); // phpcs:ignore
 	}
