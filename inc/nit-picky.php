3f2761fd46e69ca2f0dddc66a793a528dfdc18d8
Ryan Tvenge <ryan@hoverboardstudios.com>
2024-07-22 09:24:53 -0500

    Disabling SearchWP Emails.

--- inc/nit-picky.php
+++ inc/nit-picky.php
@@ -7,3 +7,8 @@
 
 // Disable WordPress Administration Email verification Screen.
 add_filter( 'admin_email_check_interval', '__return_false' );
+
+/**
+ * Disable SearchWP Email Summaries
+	*/
+add_filter( 'searchwp/email_summaries/disabled', '__return_true' );
